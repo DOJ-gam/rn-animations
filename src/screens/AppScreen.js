6aa806f8c69a3339595e5304ab5651209b38c208
DOJ <omarjeng999@gmail.com>
2022-11-27 01:36:51 +0000

    finish initials setup

---
+++ src/screens/AppScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Test from "../components/Test";
+
+const AppScreen = () => {
+  return (
+    <View>
+      <Test />
+    </View>
+  );
+};
+
+export default AppScreen;
+
+const styles = StyleSheet.create({});
